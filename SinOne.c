3a0951ea2450568762925854e6a959870c58c036
RyuminVadim <73095626+RyuminVadim@users.noreply.github.com>
2023-02-17 10:19:33 +0700

    Update SinOne.c

--- SinOne.c
+++ SinOne.c
@@ -1,7 +1,10 @@
-#include <iostream>
+#include <stdio.h>
 #include <math.h>
 #define PI 3.14159265
 #define lens 10000000
+
+float arrsin_f[lens];
+double arrsin_d[lens];
 
 void sins_float(float* arrsin) {
     for (int i = 0; i < lens; i++)
@@ -20,21 +23,16 @@ void sins_double(double* arrsin) {
     for (int i = 0; i < lens; i++)
         arrsin[i] = sin(i * PI / 180);;
 }
-void printsin_float(float* arrsin) {
-#pragma acc kernels
-    for (int i = 0; i < lens; i++)
-        std::cout << arrsin[i] << std::endl;
-}
+
 
 int main()
 {
-    float* arrsin_f = new float[lens];
-    double* arrsin_d = new double[lens];
+    
     //std::cout << lens<< std::endl;
     //std::cout << (arrsin_f);
     sins_float(arrsin_f);
     float sums = sum_sins_float(arrsin_f);
-    std::cout << sums << std::endl;
+    printf("%f", sums);
     //sins_double(arrsin_d);
 
 
