fd783f73859e7adb9d44b7cc1e5ae85d16f6020e
RyuminVadim <dflbr201@mail.ru>
2023-02-18 10:19:32 +0700

    redact

--- SinOne_float.c
+++ SinOne_float.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <math.h>
-#define PI 3.14159265
-#define lens 10000000
 
+#define lens 10000000
+#define Pi (3.14159265* 2 / lens)
+
+float sumsin = 0;
 float arrsin_f[lens];
 
 void sins_float(float* arrsin) {
 #pragma acc kernels
     for (int i = 0; i < lens; i++)
-        arrsin[i] = sin(i * PI * 2 / lens);;
+        arrsin[i] = sin(i * Pi);;
 }
 
-float sum_sins_float(float* arrsin) {
+float sum_sins(float* arrsin) {
 #pragma acc kernels
-    float sum = 0;
     for (int i = 0; i < lens; i++)
-        sum += arrsin[i];
-    return sum;
+        sumsin += arrsin[i];
+    return sumsin;
 }
 
 int main()
 {
+#pragma acc data create(arrsin_f[:lens]) copy (sumsin) copyin(Pi)
     sins_float(arrsin_f);
-    printf("%.25f", sum_sins_float(arrsin_f));
+    printf("%.25f", sum_sins(arrsin_f));
 }
