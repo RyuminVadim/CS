9b432f4028a75adde1028d206b22a43dc11e4c88
RyuminVadim <dflbr201@mail.ru>
2023-03-06 11:43:42 +0700

    red

--- Two/equation.c
+++ Two/equation.c
@@ -87,7 +87,7 @@ int main(int argc, char** argv)
 			err = 0;
 			iter++;
 			//#pragma acc parallel reduction(max:err)
-#pragma acc data present(Anew, A,err)
+#pragma acc data present(Anew, A)
 #pragma acc parallel reduction(max:err)
 			{
 #pragma acc loop independent
