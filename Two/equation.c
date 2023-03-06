05d1866cb473c49eebda3e37e8c8d482b5b441f0
RyuminVadim <dflbr201@mail.ru>
2023-03-06 11:55:50 +0700

    red

--- Two/equation.c
+++ Two/equation.c
@@ -78,16 +78,14 @@ int main(int argc, char** argv)
 	splits();
 
 	//#pragma acc data copy(err,iter) create(Anew[:sizearr * sizearr], A[:sizearr * sizearr],step) copyin(itermax,tol,sizearr)
-#pragma acc data  copyin(Anew[:sizearr * sizearr], A[:sizearr * sizearr]) create (err)
+#pragma acc data  copyin(Anew[:sizearr * sizearr], A[:sizearr * sizearr]) copy(err)
 	{
-#pragma acc update device (err)
+
 		while (iter < itermax && err>tol) {
-
-
 			err = 0;
 			iter++;
 			//#pragma acc parallel reduction(max:err)
-#pragma acc data present(Anew, A,err)
+#pragma acc data present(Anew, A)
 #pragma acc parallel reduction(max:err)
 			{
 #pragma acc loop independent
@@ -104,16 +102,10 @@ int main(int argc, char** argv)
 					err = fmax(Anew[sizearr * (i / (sizearr)) + ((i) % sizearr)] - A[sizearr * (i / (sizearr)) + ((i) % sizearr)], err);
 				}
 			}
-
-#pragma acc update host (err)
 			splits();
-			//printf("iter = %zu \t err = %f \n", iter, err);
-
-
 		}
 	}
 
-	//printf("size_arr = %zu * %zu \t iter_max= %zu \t err = %f \n", sizearr, sizearr, itermax, tol);
 	printf("iter = %zu \t err = %f \n", iter, err);
 
 	free(A);
