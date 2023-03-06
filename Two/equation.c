2161b1a6502eb7acb9237552feeb6b3f2c4590de
RyuminVadim <dflbr201@mail.ru>
2023-03-06 11:49:41 +0700

    red

--- Two/equation.c
+++ Two/equation.c
@@ -78,16 +78,16 @@ int main(int argc, char** argv)
 	splits();
 
 	//#pragma acc data copy(err,iter) create(Anew[:sizearr * sizearr], A[:sizearr * sizearr],step) copyin(itermax,tol,sizearr)
-#pragma acc data  copyin(Anew[:sizearr * sizearr], A[:sizearr * sizearr])// create (err)
+#pragma acc data  copyin(Anew[:sizearr * sizearr], A[:sizearr * sizearr]) create (err)
 	{
-//#pragma acc update device (err)
+#pragma acc update device (err)
 		while (iter < itermax && err>tol) {
 
 
 			err = 0;
 			iter++;
-			//#pragma acc parallel reduction(max:err)
-#pragma acc data present(Anew, A)
+
+#pragma acc data present(Anew, A,err)
 #pragma acc parallel reduction(max:err)
 			{
 #pragma acc loop independent
@@ -104,7 +104,7 @@ int main(int argc, char** argv)
 					err = fmax(Anew[sizearr * (i / (sizearr)) + ((i) % sizearr)] - A[sizearr * (i / (sizearr)) + ((i) % sizearr)], err);
 				}
 			}
-//#pragma acc update host (err)
+#pragma acc update host (err)
 
 			splits();
 			//printf("iter = %zu \t err = %f \n", iter, err);
