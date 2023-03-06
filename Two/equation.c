782767b6064d332d3b482bf715ec197bfabd20e8
RyuminVadim <dflbr201@mail.ru>
2023-03-06 12:26:03 +0700

    red

--- Two/equation.c
+++ Two/equation.c
@@ -106,7 +106,7 @@ int main(int argc, char** argv)
 			splits();
 
 
-		} while (iter < itermax && err>tol)
+		 } while (iter < itermax && err>tol);
 	}
 	printf("iter = %zu \t err = %f \n", iter, err);
 
