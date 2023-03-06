a2ab989a0e9e296b7a2ac990a48c983c060d7df5
RyuminVadim <dflbr201@mail.ru>
2023-03-06 11:59:37 +0700

    red

--- Two/equation.c
+++ Two/equation.c
@@ -78,9 +78,9 @@ int main(int argc, char** argv)
 	splits();
 
 	//#pragma acc data copy(err,iter) create(Anew[:sizearr * sizearr], A[:sizearr * sizearr],step) copyin(itermax,tol,sizearr)
-#pragma acc data  copyin(Anew[:sizearr * sizearr], A[:sizearr * sizearr]) copy(err)
+#pragma acc data  copyin(Anew[:sizearr * sizearr], A[:sizearr * sizearr]) create (err)
 	{
-
+#pragma acc update device(err)
 		while (iter < itermax && err>tol) {
 			err = 0;
 			iter++;
