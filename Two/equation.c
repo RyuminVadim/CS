3ea0d342dc6a94e09e92a833dc3ae620d28c0087
RyuminVadim <dflbr201@mail.ru>
2023-03-06 13:10:30 +0700

    red

--- Two/equation.c
+++ Two/equation.c
@@ -80,7 +80,7 @@ int main(int argc, char** argv)
 	//#pragma acc data copy(err,iter) create(Anew[:sizearr * sizearr], A[:sizearr * sizearr],step) copyin(itermax,tol,sizearr)
 #pragma acc data copyout(err) copyin(Anew[:sizearr * sizearr], A[:sizearr * sizearr],sizearr)
 	{
-#pragma acc kernels loop seq
+#pragma acc  loop seq
 		 do{
 
 			err = 0;
