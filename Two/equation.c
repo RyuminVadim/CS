cb0e71e58e16b9ce2c823f6cfebc55cca04eee48
RyuminVadim <dflbr201@mail.ru>
2023-03-06 11:57:48 +0700

    red

--- Two/equation.c
+++ Two/equation.c
@@ -85,7 +85,7 @@ int main(int argc, char** argv)
 			err = 0;
 			iter++;
 			//#pragma acc parallel reduction(max:err)
-#pragma acc data present(Anew, A)
+#pragma acc data present(Anew, A,err)
 #pragma acc parallel reduction(max:err)
 			{
 #pragma acc loop independent
@@ -102,7 +102,7 @@ int main(int argc, char** argv)
 					err = fmax(Anew[sizearr * (i / (sizearr)) + ((i) % sizearr)] - A[sizearr * (i / (sizearr)) + ((i) % sizearr)], err);
 				}
 			}
-#pragma acc update host(err)
+//#pragma acc update host(err)
 			splits();
 		}
 	}
