c946aaf51b6b23e2378e2d7128db0b3b8ab47dfb
RyuminVadim <73095626+RyuminVadim@users.noreply.github.com>
2023-06-01 18:02:07 +0700

    Update Mpi.cu
    исправил ошибки

--- MPI/Mpi.cu
+++ MPI/Mpi.cu
@@ -20,8 +20,8 @@ __global__ void iterate(double* A, double* A_new, size_t size_x, size_t size_y) 
 	size_t j = blockIdx.x * blockDim.x + threadIdx.x;
     size_t i = blockIdx.y * blockDim.y + threadIdx.y;
 	
-    if ((j == 0) || (i == 0) || (i == size_y - 1) || (j == size_x - 1)) return; // Don't update borders
-    A_new[i * size_x + j] = 0.25 * (A[i * size_x + j - 1] + A[(i - 1) * size_x + j] + A[(i + 1) * size_x + j] + A[i * size_x + j + 1]);	
+    if ((j > 0) && (i > 0) && (i < size_y - 1) && (j < size_x - 1))  // Don't update borders
+    	A_new[i * size_x + j] = 0.25 * (A[i * size_x + j - 1] + A[(i - 1) * size_x + j] + A[(i + 1) * size_x + j] + A[i * size_x + j + 1]);	
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -29,7 +29,8 @@ __global__ void iterate(double* A, double* A_new, size_t size_x, size_t size_y) 
 __global__ void subtraction(double* A, double* A_new, double* A_err, size_t size_x) {
 	int j = blockIdx.x * blockDim.x + threadIdx.x;
     int i = blockIdx.y * blockDim.y + threadIdx.y;
-	A_err[i * size_x + j] = A[i * size_x + j] - A_new[i * size_x + j];
+	if ((j > 0) && (i > 0) && (i < size_x - 1) && (j < size_x - 1))
+		A_err[i * size_x + j] = A[i * size_x + j] - A_new[i * size_x + j];
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////// Значения по умодчанию
@@ -44,7 +45,6 @@ int main(int argc, char** argv) {
 	sscanf(argv[2], "%d", &size);
 	sscanf(argv[3], "%d", &iter_max);
 
-	size_t totalSize = size * size;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////// Выбор видеокарт
@@ -54,32 +54,24 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &DEVICE);
     MPI_Comm_size(MPI_COMM_WORLD, &COUNT_DEVICE);
 
-	//cudaSetDevice используется для выбора устройства CUDA в соответствии с номером процесса, полученным из MPI_Comm_rank.
-	//Это позволяет каждому процессу использовать свое собственное устройство CUDA для выполнения вычислений.
     cudaSetDevice(DEVICE);
 
+    std::cout << "Number of processes: " << COUNT_DEVICE << std::endl;
     if (DEVICE == 0)
         std::cout << "Settings: " << "\n\tMin error: " << eps << "\n\tMax iteration: " << iter_max << "\n\tSize: " << size << "x" << size << std::endl;
 
-	//разрешения доступа между соседними устройствами.
-	//позволяет передавать данные напрямую между соседними устройствами без необходимости копирования через хост
-	/*
-	if (DEVICE!=0)
-        cudaDeviceEnablePeerAccess(DEVICE - 1, 0);
-    if (DEVICE!=COUNT_DEVICE-1)
-        cudaDeviceEnablePeerAccess(DEVICE + 1, 0);
-	*/
-
-	size_t size_y = size / COUNT_DEVICE + 1;
+	size_t size_y; 
+	
+	size_y = size / COUNT_DEVICE;
+	if (COUNT_DEVICE > 1)
+		size_y++;
     if (DEVICE != COUNT_DEVICE - 1 && DEVICE != 0) size_y += 1;
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////// Выделения памяти
 	double *A, *A_Device, *A_new_Device, *A_error_Device, *deviceError, *tempStorage = NULL;
     size_t tempStorageSize = 0;
 
-	//выделения памяти на хосте (CPU) с использованием управляемой памяти CUDA.
-	//Она позволяет выделить память, которая будет доступна и с CPU, и с GPU.
-    cudaMallocHost(&A, sizeof(double) * totalSize);
+    cudaMallocHost(&A, sizeof(double) * size * size);
     for (int i = 0; i < size; i++)  {
         A[i] = 10.0 + i * 10.0 / (size - 1);
         A[i * size] = 10.0 + i * 10.0 / (size - 1);
@@ -87,14 +79,13 @@ int main(int argc, char** argv) {
         A[size * (size - 1) + i] = 20.0 + i * 10.0 / (size - 1);
     }
 
-    dim3 threads(THREAD);
+    dim3 threads(THREAD,1);
     dim3 blocks(size/THREAD, size_y);
 
 	cudaMalloc(&A_Device, sizeof(double) * size * size_y);
 	cudaMalloc(&A_new_Device, sizeof(double) * size * size_y);
 	cudaMalloc(&A_error_Device, sizeof(double) * size * size_y);
 	cudaMalloc(&deviceError, sizeof(double));
-
 
 	size_t offset = (DEVICE != 0) ? size : 0;
  	cudaMemcpy(A_Device, A + (size * size * DEVICE / COUNT_DEVICE) - offset, sizeof(double) * size * size_y, cudaMemcpyHostToDevice);
@@ -113,25 +104,31 @@ int main(int argc, char** argv) {
 	while((iter < iter_max) && error > eps)	{
 		iterate<<<blocks, threads, 0, stream>>>(A_Device, A_new_Device, size, size_y);
 		iter++;
-		// Расчитываем ошибку каждую сотую итерацию
-		if (iter % UPDATE == 0) {
+		// Расчитываем ошибку каждую итерацию кратную размеру матрицы
+		if (iter % UPDATE == 0 ) {
+            
             subtraction<<<blocks, threads, 0, stream>>>(A_new_Device, A_Device, A_error_Device, size);
 			cub::DeviceReduce::Max(tempStorage, tempStorageSize, A_error_Device, deviceError, size * size_y, stream);
-			cudaMemcpyAsync(&error, deviceError, sizeof(double), cudaMemcpyDeviceToHost, datatransfer);
+            cudaStreamSynchronize(stream);
 
-			// Находим максимальную ошибку среди всех и передаём её всем процессам
-			MPI_Allreduce((void*)&error,(void*)&error, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+			if (COUNT_DEVICE > 1) {
+                cudaMemcpyAsync(&error, deviceError, sizeof(double), cudaMemcpyDeviceToHost, stream);
+                //передаём ошибку всем процессам
+				MPI_Allreduce((void*)&error, (void*)&error, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+            }
+            else{
+                cudaMemcpy(&error, deviceError, sizeof(double), cudaMemcpyDeviceToHost);
+                
+            }
 		}
 
-		//MPI_Sendrecv позволяет одновременно отправлять сообщение и принимать сообщение в одном вызове функции
-		//синхронный обмен данными между процессами
-		//MPI_Send b MPI_Recv
-		if (DEVICE != 0)                // Обмен верхней границей
+		if (DEVICE > 0)               // Обмен верхней границей
             MPI_Sendrecv(A_new_Device + size + 1, size - 2, MPI_DOUBLE, DEVICE - 1, 0, A_new_Device + 1, size - 2, MPI_DOUBLE, DEVICE - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     
-        if (DEVICE != COUNT_DEVICE - 1) // Обмен нижней границей
+        if (DEVICE < COUNT_DEVICE - 1) // Обмен нижней границей
             MPI_Sendrecv(A_new_Device + (size_y - 2) * size + 1, size - 2, MPI_DOUBLE, DEVICE + 1, 0, A_new_Device + (size_y - 1) * size + 1, size - 2, MPI_DOUBLE, DEVICE + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
+        
 		std::swap(A_Device, A_new_Device);
 	}
 
